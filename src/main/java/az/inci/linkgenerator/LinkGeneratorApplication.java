5f91112fca4868d9e2aa33248646afbc9a87253b
Museyib <museyib.e@gmail.com>
2025-04-21 11:27:43 +0400

    App icon

--- src/main/java/az/inci/linkgenerator/LinkGeneratorApplication.java
+++ src/main/java/az/inci/linkgenerator/LinkGeneratorApplication.java
@@ -3,7 +3,10 @@ package az.inci.linkgenerator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class LinkGeneratorApplication extends Application {
     @Override
@@ -11,6 +14,7 @@ public class LinkGeneratorApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(LinkGeneratorApplication.class.getResource("main-window.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Şəkillər üçün link generator");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("icon.png"))));
         stage.setScene(scene);
         stage.show();
     }
